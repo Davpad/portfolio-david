d5460082060e018f9bebf3bdf453f7a5bd46e77a
Davpad <davidpadillavillen@gmail.com>
2025-03-12 18:50:13 +0100

    Feat: added states logic to aside

--- src/Views/AboutMe.jsx
+++ src/Views/AboutMe.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./aboutMe.css"
 
 export const AboutMe = () =>{
-
+  const [view, setView] = useState ("")
+  
   return (
     <div className="App">
       <aside>
           <ul>
-            <li><p>Mis títulos</p></li>
-            <li><p>Mis cursos</p></li>
+            <li onClick={setView("Titles")}><p>Mis títulos</p></li>
+            <li onClick={setView("Courses")}><p>Mis cursos</p></li>
           </ul>
       </aside>
     </div>
