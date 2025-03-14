9f4c0989b779a82bf294a33912d6eabb64989c56
Davpad <davidpadillavillen@gmail.com>
2025-03-14 21:11:08 +0100

    Feat: added courses and titles

--- src/Views/AboutMe.jsx
+++ src/Views/AboutMe.jsx
@@ -13,8 +13,24 @@ export const AboutMe = () =>{
           </ul>
       </aside>
       <div className="about-me">
-        {view ==="Titles" && (<ul><li>Estos son mis títulos</li></ul>)}
-        {view ==="Courses" && (<ul><li>Estos son mis cursos</li></ul>)}
+        {view ==="Titles" && (<div>
+            <h1 className="header">Mis títulos</h1>
+            <ul>
+              <li>Full Stack Developer. (01/2024 - 06/2024) <i>4Geeks Academy</i></li>
+              <li>Máster de Educación y TIC. (10/2021 - 06/2023) <i>Universidad Antonio Nebrija</i></li>
+              <li>Máster en Profesorado. (10/2019 - 09/2020) <i>Universidad de Santiago de Compostela</i></li>
+              <li>Licenciado en Ingeniería de Montes. (10/2007 - 09/2015) <i>Universidad de Córdoba</i></li>
+            </ul>
+          </div>)}
+        {view ==="Courses" && (<div>
+            <h1 className="header">Mis cursos</h1>
+            <ul>
+              <li>Metodología de gestión y desarrollo de proyectos de software con scrum. (10/2024 - 11/2024) <i> Prosergen</i></li>
+              <li>IT Training (11/2024 - 12/2024) <i> Igrowker</i></li>
+              <li>Máster en Profesorado (10/2019 - 09/2020) <i> Universidad de Santiago de Compostela</i></li>
+              <li>Licenciado en Ingeniería de Montes (10/2007 - 09/2015) <i> Universidad de Córdoba</i></li>
+            </ul>
+          </div>)}
       </div>
     </div>
   );
