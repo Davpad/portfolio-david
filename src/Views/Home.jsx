a1a39c42cf0517d10eb4ab6b4b4f3c6a5d20ac1b
Davpad <davidpadillavillen@gmail.com>
2025-03-21 20:45:45 +0100

    Feat: added home layout

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,9 +1,18 @@
+import React from "react";
+import "./home.css"
+import Yo from "../img/1735833010091.jpg"
 
 export const Home = () =>{
 
   return (
     <div className="App">
-      Home
+      <div className="home-body">
+        <img src={Yo} alt="David" title="David"/>
+        <div>
+          <h1>¡¡Bienvenidos a mi Portfolio!!</h1>
+          <p>Soy David Padilla, un Full Stack Developer apasionado por la docencia y la naturaleza </p>
+        </div>
+      </div>
     </div>
   );
 }
