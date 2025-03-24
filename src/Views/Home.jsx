eb211705be8aba807a013192a18c3bcbfd1f09eb
Davpad <davidpadillavillen@gmail.com>
2025-03-24 18:41:00 +0100

    Feat: initial information in home

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -10,7 +10,7 @@ export const Home = () =>{
         <img src={Yo} alt="David" title="David"/>
         <div>
           <h1>¡¡Bienvenidos a mi Portfolio!!</h1>
-          <p>Soy David Padilla, un Full Stack Developer apasionado por la docencia y la naturaleza </p>
+          <p>Soy David Padilla, un Full Stack Developer apasionado por la docencia y la naturaleza. Mis conocimientos de ingeniería me permiten entender la lógica que hay detrás de las diferentes dinámicas en una página web y el flujo de información que se genera entre backend y frontend. Además mi inteligencia especial me permite desarrollar layouts originales y aportar soluciones únicas. Mi experiencia como docente me permite localizar fácilmente los problemas en el código y aportar soluciones innovadoras y creativas. </p>
         </div>
       </div>
     </div>
