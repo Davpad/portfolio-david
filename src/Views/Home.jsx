114ee0c3b8b2b5eff38c902aca92fa9876e25f2a
Davpad <davidpadillavillen@gmail.com>
2025-03-28 19:32:05 +0100

    Feat: added more information in home

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import "./home.css"
 import Yo from "../img/1735833010091.jpg"
 
 export const Home = () => {
+
+  const [view, setView] = useState ("")
 
   return (
     <div className="App">
@@ -17,9 +19,18 @@ export const Home = () => {
              layouts originales y aportar soluciones únicas. Mi experiencia como docente me permite 
              localizar fácilmente los problemas en el código y aportar soluciones innovadoras y creativas. </p>
           <div className="button-line">
-            <button className="button"><i class="fa-solid fa-plus"></i></button>
+            <button className="button" onClick={()=>setView("show")}><i class="fa-solid fa-plus"></i></button>
           </div>
         </div>
+      </div>
+      <div className="home-body">
+        {view==="show"&&(<p>Veo que estás interesado en mi Portfolio. ¡Es altamente interactivo!.
+           En el navbar de arriba podrás localizar más información sobre mis proyectos y mi experiencia. 
+           El apartado sobre mi contiene información sobre mis títulos y cursos, 
+           las tecnologías que utilizo y las hablidades que he ido adquiriendo a lo largo de mi camino
+            como desarrollador. Algunos apartados son altamente interactivos y contienen botones que 
+            nos dirigen a videos de presentación, al repositorio de Github o a la propia página web. 
+            ¡Seguid los botones verdes!</p>)}
       </div>
     </div>
   );
