7562c2cab2b5fe7a823fd806929c1df3a2a582d4
Davpad <davidpadillavillen@gmail.com>
2025-04-01 14:12:51 +0200

    Style: styling aside footer and navbar

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,9 +7,9 @@ export const Footer = () =>{
 
     return (
         <footer>
-            <div>
-               <Link to="https://github.com/Davpad"><i className="fa-brands fa-github"></i></Link>
-               <Link to="https://www.linkedin.com/in/david-padilla-vill%C3%A9n/"><i className="fa-brands fa-linkedin"></i></Link>
+            <div className="footer-content">
+               <div className="footer-link"><Link to="https://github.com/Davpad" className="btn-text"><i className="fa-brands fa-github"></i></Link></div>
+               <div className="footer-link"><Link to="https://www.linkedin.com/in/david-padilla-vill%C3%A9n/" className="btn-text"><i className="fa-brands fa-linkedin"></i></Link></div>
             </div>
         </footer>
     );
