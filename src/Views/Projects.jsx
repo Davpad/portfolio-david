c0cc1a6a7f7e633530830168cb0f3c32f9392ab9
Davpad <davidpadillavillen@gmail.com>
2025-04-14 19:32:21 +0200

    Feat: added summart in projects

--- src/Views/Projects.jsx
+++ src/Views/Projects.jsx
@@ -39,6 +39,15 @@ export const Projects = () =>{
               <a href="https://youtu.be/SrZRXFhTtzs" className="button"><i className="fa-solid fa-video"></i></a>
               <a href="https://github.com/Davpad/proyecto-final-entubarrio" className="button"><i className="fa-brands fa-github"></i></a>
             </div>
+            <div className="project-text">
+              <p>Este proyecto se centra en la idea de crear un e-commerce para comercios de barrio. 
+                Los vendedores por un lado son capaces de crear sus tiendas y añadir sus productos. 
+                Mientras que los compradores o usuarios pueden buscar información de productos que 
+                les interesen, contrastar precios o seleccionar tiendas especializadas en la zona. 
+                Se ha utilizado <b>Flask</b> y <b>Python</b> para el diseño del backend, y <b>JavaScript</b> aplicado a <b>React</b>
+                 para el frontend, junto con <b>Bootstrap</b> para el diseño. Se trata de un proyecto colaborativo
+                 en el que tuve que desarrollar diferentes habilidades junto a un equipo de 4 personas.</p>
+            </div>
 
 
           </div>)}
@@ -48,8 +57,6 @@ export const Projects = () =>{
               <div className="tools-line">
                 <i className="fa-brands fa-html5 tool-icon"></i>
                 <i className="fa-brands fa-css3-alt tool-icon"></i>
-                <i className="fa-brands fa-js tool-icon"></i>
-                <i className="fa-brands fa-bootstrap tool-icon"></i>
                 <i className="fa-brands fa-react tool-icon"></i>
               </div>
             </div>
@@ -64,23 +71,17 @@ export const Projects = () =>{
               <a href="https://youtube.com/shorts/0gUeS3i0ihE" className="button"><i className="fa-solid fa-video"></i></a>
               <a href="https://github.com/Davpad/i004-pawsome-front" className="button"><i className="fa-brands fa-github"></i></a>
             </div>
+            <div className="project-text">
+              <p>Este proyecto trata de crear una página web para la gestión de protectoras 
+                de animales. Permite añadir los diferentes animales, ayudas monetarias y en 
+                especie, gestión de voluntarios y de adopciones. Además el resto de usuarios 
+                son capaces de filtrar los animales por especie o por las diferentes protectoras. 
+                Se trata de un proyecto colaborativo con un equipo multidisciplinar de más de 
+                15 personas. Yo formé parte del equipo de frontend, donde estuvimos trabajando 
+                con <b>TypeScript</b> aplicado a <b>React</b>, utilizando <b>Redux</b> para la
+                 gestión del contexto y <b>Tailwind</b> para el modelado.</p>
+            </div>
 
-
-          </div>)}
-          {view ==="Habilities" && (<div>
-            <h1 className="header">Mis habilidades</h1>
-            <ul>
-              <li>Modelado de páginas dinámicas utilizando <b>HTML, CSS, Bootstrap y Tailwind</b></li>
-              <li>Implementación de funcionalidades utilizando <b>React y Redux</b></li>
-              <li>Diseño de bases de datos utilizando <b>SQL Alchemy </b></li>
-              <li>Formulación de lógicas intuitivas usando  <b>JavaScript y TypeScript</b></li>
-              <li>Construcción de rutas de Backend mediante  <b>Python y Flask</b></li>
-              <li>Autenticación a través de <b>JWT</b></li>
-              <li>Control de versiones y aprobación de PR mediante <b>Git y GitHub</b></li>
-              <li>Testeo de API REST mediante <b>Postman y ThunderClient</b></li>
-              <li>Uso de APIs externas para añadir funcionalidades a las aplicaciones</li>
-
-            </ul>
           </div>)}
       </div>
     </div>
